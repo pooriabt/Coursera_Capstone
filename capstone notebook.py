08f9156767fe00a154b42049db320f3694d54648
pooriabt <76968679+pooriabt@users.noreply.github.com>
2021-01-05 21:45:51 +0330

    Add files via upload

---
+++ capstone notebook.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# this notebook will be mainly used for the capstone project.
+
+# In[3]:
+
+
+import pandas as pd
+import numpy as np
+print("Hello Capstone Project Course!")
+
